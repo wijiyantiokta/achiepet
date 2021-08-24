a726921c4e7dd47b62ce16b6475cfa307a3e0f51
Wijiyanti Nur Oktawia <39021816+wijiyantiokta@users.noreply.github.com>
2021-08-24 21:25:25 +0700

    Update v_footer.php

--- application/views/grooming/layout/v_footer.php
+++ application/views/grooming/layout/v_footer.php
@@ -74,13 +74,6 @@
   </div>
 </footer>
 
-<!-- <div class="widget HTML hidden-print" data-version="1" id="HTML10">
-	<div class="widget-content">
-		<div style="display:scroll;position:fixed;bottom:12px;left:24px;z-index:1000;"><a href="https://api.whatsapp.com/send?phone=6287700216574&amp;text=Hallo%20admin,%20Saya%20ingin%20memesan%20catering.%20Bagaimana%20cara%20ordernya ?" target="_blank"><img border="0" src="<?= base_url() ?>public/media/icon/wa.png" height="65" width="170"></a></div>
-	</div>
-	<div class="clear"></div>
-	<div class="clear"></div>
-</div> -->
 
 
 <!-- loader -->
